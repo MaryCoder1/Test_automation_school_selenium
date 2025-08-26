08484f4bfc987490eecaa7adfe4bec8cd5f961ae
Mary Afolayan <maryadebimpe017@gmail.com>
2025-08-26 21:53:18 +0100

    pushing java files

---
+++ Module 4/Task/Task 1/Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is my first project");
+        System.out.println("It's selenium guys!");
+        System.out.println("I am excited");
+        System.out.println("can't wait");
+        System.out.println("Quality assurance engineer");
+
+    }
+}
