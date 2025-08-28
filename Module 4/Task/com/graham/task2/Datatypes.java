79216aa67b4d8342deaf4885f6370499bec92c35
Mary Afolayan <maryadebimpe017@gmail.com>
2025-08-28 23:15:30 +0100

    Module 2 - Datatypes

---
+++ Module 4/Task/com/graham/task2/Datatypes.java
@@ -0,0 +1,29 @@
+package com.graham.task2;
+
+public class Datatypes {
+    public static void main(String[] args) {
+        //byte -128 to 127
+        byte age = 125;
+        //short -32768 to 32767
+        short money = 32500;
+        //int from -2,147,483,648 to 2,147,483,647
+        int isplenty = 214_748_364;
+        //long from -9,223,372,036,854,775,808   to   9,223,372,036,854,775,807
+        long trillionaire = 9_000_000_000_000L;
+        //float from 6 to 7 decimal places
+        float grade = 97.2508f;
+        //double from 15 to digit digits
+        double estimate = 1.2111111111111;
+        //char i.e simple character
+        char Symbol = 'M';
+        //boolean means true or false
+        boolean answer = true;
+        System.out.println(trillionaire);
+        System.out.println(grade);
+        System.out.println(estimate);
+        System.out.println(Symbol);
+        System.out.println(answer);
+
+
+    }
+}
