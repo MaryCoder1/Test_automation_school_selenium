536df46dca95eb121fd8fa44e4daf776d15a6c9f
Mary Afolayan <maryadebimpe017@gmail.com>
2025-08-31 17:35:45 +0100

    pushing for task 4

---
+++ Module 4/Task/com/graham/task4/ArithmeticOperatorsOp.java
@@ -0,0 +1,11 @@
+package com.graham.task4;
+
+public class ArithmeticOperatorsOp {
+    public static void main(String[] args) {
+        double radius = 12.7;
+        double area = Math.PI * radius * radius;
+        String result = area + "m";
+        System.out.println(result);
+
+    }
+}
